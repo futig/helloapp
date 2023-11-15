b85e1a73fca0de7b255a04f96e87f87f6919f039
black <black@example.com>
2023-11-15 21:59:49 +0500

    Change message

--- GitActually/GitActually/Program.cs
+++ GitActually/GitActually/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("hello");
+        Console.WriteLine("hello everyone");
     }
     
     public static void GreetWhite()
